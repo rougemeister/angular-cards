d64ec3bf6b97c9218172de69bbbeb54e5b9e9a0f
rougemeister <rougemeister1@gmail.com>
2024-07-23 06:14:51 +0000

    Added profile card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ECommerceComponent } from "./components/e-commerce/e-commerce.component";
 import { NewsArticleComponent } from "./components/news-article/news-article.component";
 import { EventCardComponent } from './components/event-card/event-card.component';
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ECommerceComponent, NewsArticleComponent,EventCardComponent],
+  imports: [RouterOutlet, ECommerceComponent, NewsArticleComponent,EventCardComponent, ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
