e35bbff71514b9d3138c14d2c6a699a631567ece
rougemeister <rougemeister1@gmail.com>
2024-07-23 04:46:18 +0000

    Added global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ECommerceComponent } from "./components/e-commerce/e-commerce.component";
+import { NewsArticleComponent } from "./components/news-article/news-article.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ECommerceComponent, NewsArticleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
